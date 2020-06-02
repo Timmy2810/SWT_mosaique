852a6c166149cb2d611d1cd4223db0b245c91de7
Timmy2810 <52511392+Timmy2810@users.noreply.github.com>
2020-06-02 17:59:12 +0200

    Ub 3 A2c

--- iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCoursePlugin.java
+++ iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCoursePlugin.java
@@ -1,11 +1,19 @@
 import org.iMage.plugins.PluginForJmjrst;
 import org.jis.Main;
 
+import java.util.List;
+
 public class JavaCrashCoursePlugin extends PluginForJmjrst {
 
     private String name;
     private int numberOfParaemters;
     private Main m;
+
+    public JavaCrashCoursePlugin() {
+        List javaReleases = List.of(
+            "Java SE 8", "Java SE 9", "Java SE 10", "Java SE 11", "Java SE 12", "Java SE 13", "Java SE 14");
+        numberOfParaemters = javaReleases.size();
+    }
 
     @Override
     public String getName() {
@@ -36,4 +44,6 @@ public class JavaCrashCoursePlugin extends PluginForJmjrst {
     public void configure() {
 
     }
+
+
 }
